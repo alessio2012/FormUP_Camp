d0e5fbec82abbfd286f2f2647a4040729f728c67
alessio2012 <48069116+alessio2012@users.noreply.github.com>
2025-05-05 19:39:51 +0200

    Aggiunto il sistema di accesso tramite login e password

---
+++ src/main/java/formup/utilities/UserToken.java
@@ -0,0 +1,8 @@
+package formup.utilities;
+
+public class UserToken {
+	
+	public static final String ADMIN = "s9laPAHICdpoq4RsKx8RBZSS"; 
+	public static final String COMMON = "1oWWtgpe9QHEJ0caV2zifhFo"; 
+
+}
